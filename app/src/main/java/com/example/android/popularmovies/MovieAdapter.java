8a30e89c96fea4275f9f33a9289235140335f443
Gregor <gregorlennie@hotmail.co.uk>
2020-03-31 13:04:50 +0100

    Adds test code for adapter and view holder for RecyclerView

---
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -0,0 +1,50 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+    private final int NUM_OF_ITEMS = 20;
+
+    @NonNull
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int idOfView = R.layout.test_item;
+
+        boolean shouldAttachImmediatelyToParent = false;
+
+        View view = LayoutInflater.from(context).inflate(idOfView, parent, shouldAttachImmediatelyToParent);
+        MovieAdapterViewHolder viewHolder = new MovieAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapterViewHolder holder, int position) {
+        String text = String.valueOf(position);
+        holder.mTestItemTextView.setText(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return NUM_OF_ITEMS;
+    }
+
+    public static class MovieAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTestItemTextView;
+        public MovieAdapterViewHolder(View view) {
+            super(view);
+            mTestItemTextView = view.findViewById(R.id.tv_test_item);
+        }
+    }
+
+}

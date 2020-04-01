df09fb9bab6ae7ece89f99b9f4555ee6295b9373
Gregor <gregorlennie@hotmail.co.uk>
2020-04-01 08:55:47 +0100

    Creates Movie class for storing movie details

--- app/src/main/java/com/example/android/popularmovies/data/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/data/Movie.java
@@ -2,5 +2,38 @@ package com.example.android.popularmovies.data;
 
 public class Movie {
 
-    // TODO (1) Add attributes and constructor for a Movie object
+    // COMPLETED (1) Add attributes and constructor for a Movie object
+    private String title;
+    private String releaseDate;
+    private String image;
+    private String plot;
+    private int rating;
+
+    public Movie(String title, String releaseDate, String image, String plot, int rating) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.image = image;
+        this.plot = plot;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
